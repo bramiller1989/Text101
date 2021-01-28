99675679dd1f75cdd653c0ad28a8be6970c8f19d
bramiller1989 <bramiller1989@gmail.com>
2021-01-27 21:07:53 -0600

    Add textArea in unity ui

--- Assets/State.cs
+++ Assets/State.cs
@@ -5,5 +5,5 @@ using UnityEngine;
 [CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject
 {
-    [SerializeField] string storyText;
+    [TextArea(10,14)] [SerializeField] string storyText;
 }
