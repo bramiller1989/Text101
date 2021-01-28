cd13a7bed27ecd0d7543ab011ca047994a6e57f7
bramiller1989 <bramiller1989@gmail.com>
2021-01-27 21:04:49 -0600

    add storyText field and add to create asset menu

--- Assets/State.cs
+++ Assets/State.cs
@@ -2,7 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[CreateAssetMenu(menuName = "State")]
 public class State : ScriptableObject
 {
-    
+    [SerializeField] string storyText;
 }
